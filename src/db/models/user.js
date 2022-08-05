6ffd45a98884088c9682bc8a058da7ecc118d6e5
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-05 14:30:38 +0200

    Jwt middleware and tools

---
+++ src/db/models/user.js
@@ -0,0 +1,33 @@
+import sequelize from "../index.js";
+import { DataTypes } from "sequelize";
+import bcrypt from "bcrypt";
+
+const User = sequelize.define(
+  "user",
+  {
+    id: {
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+},
+  {
+    timestamps: true,
+  }
+);
+
+
+
+export default User;
