f6646413d40b2fe97389bf9c2ed3649d379aacc4
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-06 16:49:24 +0200

    get todos for a specific user

--- src/services/todos/index.js
+++ src/services/todos/index.js
@@ -10,11 +10,21 @@ import {
   todosRouter.post("/", JWTAuthMiddleware,  async (req, res, next) => {
     try {
       const newNote = await Todo.create(req.body);
-      res.send(newNote);
+      res.status(201).send(newNote);
     } catch (error) {
       console.log(error);
       next(error);
     }
   });
+
+    todosRouter.get("/:userId", JWTAuthMiddleware, async (req, res, next) => {
+    try {
+        const todos = await Todo.findAll({ where: { userId: req.params.userId } });
+        res.status(201).send(todos);
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+  })
 
   export default todosRouter;
