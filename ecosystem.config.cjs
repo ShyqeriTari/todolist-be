6f53709fc7712ba2d6f7215f101c8bb7beef3f27
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-12 15:30:20 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,26 +1,32 @@
 module.exports = {
-  apps : [{
+  apps: [{
     name: 'todolist-be',
     script: './src/index.js',
     watch: true,
     env: {
-      NODE_ENV: 'development'
+      PORT: "42069",
+      HOST: "15.160.205.92",
+      PASSWORD: "89^eqCR*_p_Ua%g5",
+      JWT_SECRET: "todolist_challenge_silvio",
     },
     env_production: {
-      NODE_ENV: "production",
+      PORT: "42069",
+      HOST: "15.160.205.92",
+      PASSWORD: "89^eqCR*_p_Ua%g5",
+      JWT_SECRET: "todolist_challenge_silvio",
     }
   }],
 
-  deploy : {
-    production : {
-      user : 'ubuntu',
+  deploy: {
+    production: {
+      user: 'ubuntu',
       key: '~/Downloads/key.pem',
-      host : 'silvio.tech-challenges.toduba.it ',
-      ref  : 'origin/main',
-      repo : 'https://github.com/ShyqeriTari/todolist-be',
-      path : '/home/ubuntu/todolist-be-c',
+      host: 'silvio.tech-challenges.toduba.it ',
+      ref: 'origin/main',
+      repo: 'https://github.com/ShyqeriTari/todolist-be',
+      path: '/home/ubuntu/todolist-be-c',
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install && npm run start && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && npm run start && pm2 reload ecosystem.config.js --env production',
       'pre-setup': ''
     }
   }
