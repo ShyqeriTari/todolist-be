3b5e4eb1489473de64c2fce0db4ad66060b7e20e
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-12 15:31:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ server.use("/user", usersRouter);
 server.use("/todo", todosRouter);
 server.use("/docs", swaggerUI.serve, swaggerUI.setup(yamlDocument))
 
-const PORT = 5000
+const PORT = 5001
 
 server.use(badRequestHandler);
 server.use(unauthorizedHandler);
