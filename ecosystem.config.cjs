68ec491061a5c7e0ba05a96582e245247f93c282
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-12 11:38:15 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -40,7 +40,7 @@ module.exports = {
 
         'pre-deploy-local': "echo 'This is a local executed command'",
 
-        'post-deploy': "npm install && pm2 reload ecosystem.config.js --env production",
+        'post-deploy': "npm install && pm2 reload ecosystem.config.cjs --env production",
       },
     }
   }
