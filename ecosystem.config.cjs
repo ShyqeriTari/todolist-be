ed517ec3327f0a1083f3858501adf47d45d2a7c2
Shyqeri Tari <63459350+ShyqeriTari@users.noreply.github.com>
2022-08-12 15:44:39 +0200

    Update ecosystem.config.cjs

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,19 @@ module.exports = {
   apps: [{
     name: 'todolist-be',
     script: './src/index.js',
-    watch: true
+    watch: true,
+    env_production: {
+      PORT: "42069",
+      HOST: "15.160.205.92",
+      PASSWORD: "89^eqCR*_p_Ua%g5",
+      JWT_SECRET: "todolist_challenge_silvio",
+    },
+    env_development: {
+      PORT: "42069",
+      HOST: "15.160.205.92",
+      PASSWORD: "89^eqCR*_p_Ua%g5",
+      JWT_SECRET: "todolist_challenge_silvio",
+    }
   }],
 
   deploy: {
@@ -15,13 +27,7 @@ module.exports = {
       path: '/home/ubuntu/todolist-be-c',
       'pre-deploy-local': '',
       'post-deploy': 'npm install && npm run start && pm2 reload ecosystem.config.js --env production',
-      'pre-setup': '',
-      env: {
-        PORT: "42069",
-        HOST: "15.160.205.92",
-        PASSWORD: "89^eqCR*_p_Ua%g5",
-        JWT_SECRET: "todolist_challenge_silvio",
-      },
+      'pre-setup': ''
     }
   }
 };
